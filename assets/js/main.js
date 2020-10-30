f6b3e6fd9e545aa31a9b355c397dd5797ba0eddf
alexsaranin <alex_saranin@list.ru>
2020-10-30 16:25:31 +0300

    lesson 5

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,54 @@ btnTop.addEventListener('click', function (event) {
 function toggleShowTooltip(tooltip, isShow) {
 
 }
+
+
+const originBolt = document.querySelector('.bolt-inner__img')
+
+
+// const colorBtnRed = document.querySelector('.btn-color_red')
+// colorBtnRed.addEventListener('click', changeColor)
+//
+// const colorBtnYellow = document.querySelector('.btn-color_yellow')
+// colorBtnYellow.addEventListener('click', changeColor)
+
+const colorBtnsPseudo = document.querySelectorAll('.fastener-colors__btn')
+const colorBtns = Array.from(colorBtnsPseudo)
+
+// Способ 1
+// for (let i = 0; i < colorBtns.length; i++) {
+//   colorBtns[i].addEventListener('click', changeColor)
+// }
+
+console.log(colorBtns)
+console.log([1, 2])
+
+// Способ 2.1
+colorBtns.forEach(function (btn) {
+  btn.addEventListener('click', changeColor)
+})
+
+
+// const numbers = [5, 32, 7, 234]
+//
+// numbers.forEach(function (item, index) {
+//   console.log(item)
+// })
+
+// Способ 2.2 эквивалентен
+// const forEach = function (array, cb) {
+//   for (let i = 0; i < array.length; i++) {
+//     cb(array[i], i)
+//   }
+// }
+//
+// forEach([1, 5, 6, 2], function (item, index) {
+//   console.log(item, index)
+// })
+
+// Способ 3 - делегирование событий
+
+
+function changeColor(event) {
+  originBolt.src = this.dataset.src
+}
