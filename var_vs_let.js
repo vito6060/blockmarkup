a12e8481ee04acbe3c7f115fb7d95a631cd6d5ac
alexsaranin <alex_saranin@list.ru>
2020-11-04 17:13:18 +0300

    lesson 8

---
+++ var_vs_let.js
@@ -0,0 +1,31 @@
+// var array = []
+
+
+
+// for(var i = 0; i < 11; i = i + 2) {
+//    замыкание
+//   (function (i){
+//     array.push(function () {
+//       console.log(i)
+//     })
+//   }(i))
+// }
+
+// console.log(i) // i = 10
+
+// console.log(array)
+
+// var func = array[0]
+//
+// func()
+
+
+var a = 0
+var b = 3
+
+
+function sum() {
+  return a + b
+}
+
+console.log()
